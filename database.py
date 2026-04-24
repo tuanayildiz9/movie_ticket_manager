43f078cd15044c0eec95a2218286909f50315b69
tuanayildiz9 <tuana.yildiz@students.fhnw.ch>
2026-04-24 14:35:59 +0200

    project structure

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Session, create_engine
+
+DATABASE_URL = "sqlite:///my_app.db"
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
